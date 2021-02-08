0c6f534fe031f70882c6c41ed1917221721377ea
Micheal <38526063+kainesmicheal@users.noreply.github.com>
2021-02-08 14:07:10 +0530

    Create query11.js

---
+++ regex/query11.js
@@ -0,0 +1,18 @@
+\\https://content.codecademy.com/courses/regex/build/index_all.html?query=11
+
+(\d\s|\()?(\d{3})(-|\s|.)(5+)(-|\s|.)?(\d{4})
+
+(..?)?(\d{3})(.)(5+)(.)?(\d+)
+
+/*group1 - take anything + anything(optional)
+
+group2 - number{3}
+
+group3 - anything
+
+group4 - 5 atleast once and more
+
+group5 - anything
+
+group6 - number more */
+
