0ca5b943210c30711c47456921273ab5c378b157
kainesmichea <keinesmicheal@gmail.com>
2021-05-10 23:29:59 -0400

    updated notes

--- machines/webgoat/run.sh
+++ machines/webgoat/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -p 127.0.0.1:9001:8080 -p 127.0.0.1:9002:9090 -e TZ=Europe/Amsterdam webgoat/goatandwolf
+docker run -p 127.0.0.1:9080:8080 -p 127.0.0.1:9081:9090 -e TZ=Europe/Amsterdam webgoat/goatandwolf
